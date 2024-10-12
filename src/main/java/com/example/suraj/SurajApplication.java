36691b3e4481e42b85d14fe62ba250027000daac
surajchauhan2310 <23surajrc@gmail.com>
2024-10-12 15:55:55 +0530

    First java commit

---
+++ src/main/java/com/example/suraj/SurajApplication.java
@@ -0,0 +1,16 @@
+package com.example.suraj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurajApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+
+		SpringApplication.run(SurajApplication.class, args);
+	}
+
+}	
+
---
+++ src/test/java/com/example/suraj/SurajApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.suraj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurajApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
